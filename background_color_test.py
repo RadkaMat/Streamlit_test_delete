d6a0abd1e903f67ba91903769d6ae41a9de72bc9
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-12-29 21:40:17 +0100

    Update background_color_test.py
    Upgrade - add background image

--- background_color_test.py
+++ background_color_test.py
@@ -16,6 +16,7 @@ TEXT = """
 NameError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
 """
 
+st.markdown(PAGE_BG_STYLE, unsafe_allow_html=True)
 st.title("Big Title")
 
 st.write(TEXT)
